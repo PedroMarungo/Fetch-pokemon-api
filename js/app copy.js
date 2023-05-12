a8c3f0682d72c98d5e271809baacfad31d1908b6
Pedro Marungo <pedromarungo@Pedros-Mac-mini.local>
2023-05-12 00:17:07 -0500

    added function with fetch call to pokemon api

--- js/app copy.js
+++ js/app copy.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  /**
+   * Function to fetch Pokemon data
+   * Fetches Pokemon data from the API.
+   * Sends a fetch web request to the Pokemon API and receives a JSON-like object with 151 Pokemon URLs.
+   * Extracts the array of Pokemon objects from the response data and iterates through them.
+   * Calls the getPokemon function for each individual Pokemon.
+   */
+  const fetchPokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => res.json())
+      .then((data) => data.results.forEach(getPokemon));
+  };
+});
