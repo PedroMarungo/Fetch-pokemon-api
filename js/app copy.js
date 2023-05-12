28173ae167de727cafa3358dd0f977befc7c2c89
Pedro Marungo <pedromarungo@Pedros-Mac-mini.local>
2023-05-12 00:21:32 -0500

    added fuction that redends fetch api data and displays to the dom

--- js/app copy.js
+++ js/app copy.js
@@ -26,4 +26,37 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((res) => res.json())
       .then(renderPokemon);
   };
+  /**
+   * Function to render Pokemon data
+   * Renders Pokemon data.
+   * Takes a Pokemon object and creates a card element with relevant data.
+   * Appends the card to the card container in the DOM.
+   */
+  const renderPokemon = (poke) => {
+    const card = document.createElement("li");
+    card.classList.add("card");
+    card.innerHTML = `
+      <img src="${poke.sprites.front_default}" />
+      <h3>${poke.name.toUpperCase()}</h3>
+      <div class='card-info'>
+        
+        <p> # ${poke.order}</p>
+        <p>Type: ${poke.types[0].type.name}</p>
+        <p>Weight: ${poke.weight}</p>
+        <p>Height: ${poke.height}</p>
+       
+      </div>
+    `;
+    const header = document.querySelector("header");
+    header.addEventListener("mouseover", () => {
+      header.classList.toggle("shine");
+    });
+    document.querySelector(".card-container").appendChild(card);
+    card.addEventListener("click", () => {
+      card.classList.toggle("holo");
+    });
+  };
+
+  // Call the fetchPokemon function to initiate data retrieval
+  fetchPokemon();
 });
